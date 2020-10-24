c332d171e78a9652e839c8b6155d38f749014053
Sharin Dmitry <shdimma@gmail.com>
2020-10-24 17:52:37 +0800

    Return proper HTTP codes

--- src/index.php
+++ src/index.php
@@ -7,13 +7,15 @@ echo "Under developing.Method is $method. Path is $path \n";
 
 if($method == 'GET' && $path == "/v1/todos")
 {
-    echo "Todo list under developing\n";
+    ///echo "Todo list under developing\n";
+    http_response_code(501);
 
 } else if($method == 'GET' && preg_match('|^/v1/todos/(\d{1,9})$|', $path, $matches))
 {
     $id = $matches[1];
-    echo "Todo item with ID = $id .Under developing\n";
+    //echo "Todo item with ID = $id .Under developing\n";
+    http_response_code(501);
 }
 else {
-    echo "Not found. \n";
+    http_response_code(404);
 }
